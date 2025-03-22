23de6cceb5c607afb8923afcda18b8f82fb87b67
Jessica <jesss8692@hotmail.com>
2025-03-22 20:34:57 +0100

    Add books API to fetch books

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  
+  const [books, setBooks] = useState([])
+  const url = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
+
+  console.log(books)
+  useEffect( () => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url) 
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`)
+        }
+        const data = await response.json()
+        setBooks(data.docs)
+      } catch (error)  {
+        console.log(error.message)
+      }
+    }
+    fetchData()
+  }, [])
+  
   return (
     <>
     </>
