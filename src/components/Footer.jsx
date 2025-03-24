97c646c3b593f1dd75717f342d26487f167c31f4
Jessica <jesss8692@hotmail.com>
2025-03-24 13:38:03 +0100

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return  (
+        <footer className="fixed bottom-0 w-full text-center py-4">
+            <span>&copy; Created by Bookworms & Co</span>
+        </footer>
+    )
+}
--- src/layout/LayoutRoute.jsx
+++ src/layout/LayoutRoute.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
+
 export default function LayoutRoute() {
     return(
         <> 
             <Header/>
             <Outlet/>
+            <Footer/>
         </>
     )
 }
