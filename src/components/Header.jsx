b53ec2a398e68204077c47c8fdbe694a6e25e3d3
Jessica <jesss8692@hotmail.com>
2025-03-24 11:49:14 +0100

    Add window eventlistener to change nav depending on size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 import NavMobile from './NavMobile'
 import NavDesktop from './NavDesktop'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
-const navItems = ['Home', 'Search', 'Want to read', 'read', 'Login']
+
+    const navItems = ['Home', 'Search', 'Want to read', 'read', 'Login']
+
+    const [windowSize, setWindowSize] = useState(window.innerWidth) 
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    },[])
 
     return(
         <header>
-            {/* <NavMobile navItems={navItems}/> */}
-            <NavDesktop navItems={navItems}/>
+            {windowSize < 800 ? 
+            <NavMobile navItems={navItems}/> :
+            <NavDesktop navItems={navItems}/>}
         </header>
     )
 }
