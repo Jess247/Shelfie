26277cd485daaeb2935bd6c8822eaee791669ef4
Jessica <jesss8692@hotmail.com>
2025-03-24 11:31:47 +0100

    Add responsive desktop navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import NavMobile from './NavMobile'
+import NavDesktop from './NavDesktop'
 
 export default function Header() {
 const navItems = ['Home', 'Search', 'Want to read', 'read', 'Login']
 
     return(
         <header>
-            <NavMobile navItems={navItems}/>
+            {/* <NavMobile navItems={navItems}/> */}
+            <NavDesktop navItems={navItems}/>
         </header>
     )
 }
---
+++ src/components/NavDesktop.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { Link } from "react-router"
+
+export default function NavDesktop({navItems}) {
+    
+    const [isToggled, setIsToggled] = useState(false)
+    
+    const handleClick = () => {
+        setIsToggled(!isToggled)
+    }
+
+    return(
+        <nav className="flex gap-4 mt-4 justify-between w-[90%] mx-auto">
+            <span>Logo</span>
+            <div className="flex justify-center items-center gap-4 ">
+                {navItems.map(item => <Link className={`hover:underline underline-offset-8 hover:text-[#D90368]`} to="/">{item}</Link>)}
+            </div>
+        </nav>
+    )
+}
