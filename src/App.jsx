1578164106763581ba610977de74204b21adc88c
Jessica <jesss8692@hotmail.com>
2025-03-24 20:15:38 +0100

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Carousel from './components/CarouselComponent'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router'
 
 function App() {
@@ -9,27 +10,36 @@ function App() {
   const [books, setBooks] = useState([])
   const url = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
 
-  // console.log(books)
-  // useEffect( () => {
-  //   async function fetchData() {
-  //     try {
-  //       const response = await fetch(url) 
-  //       if (!response.ok) {
-  //         throw new Error(`Response status: ${response.status}`)
-  //       }
-  //       const data = await response.json()
-  //       setBooks(data.docs)
-  //     } catch (error)  {
-  //       console.log(error.message)
-  //     }
-  //   }
-  //   fetchData()
-  // }, [])
+  const topFive = () => {
+    const booksObj = []
+    for (let i = 0; i < 5; i++) {
+      booksObj.push(books[i])
+    }
+    return booksObj
+  }
+
+  console.log(books)
+  useEffect( () => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url) 
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`)
+        }
+        const data = await response.json()
+        setBooks(data.docs)
+      } catch (error)  {
+        console.log(error.message)
+      }
+    }
+    fetchData()
+  }, [])
   
   return (
    <>
    <Outlet/>
    <Hero/>
+   <Carousel books={topFive}/>
    </>
   )
 }
