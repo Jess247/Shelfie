677e14a28c535b4412d40f8eb19f80d2783bb720
Jessica <jesss8692@hotmail.com>
2025-03-24 11:20:26 +0100

    Add dark mode

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -17,13 +17,15 @@ export default function NavMobile({navItems}) {
             <IoClose 
                 onClick={handleClick}
                 size={25}
+                className="hover:text-[#D90368]"
             />:
             <RxHamburgerMenu 
                 onClick={handleClick} 
                 size={25}
+                className="hover:text-[#D90368]"
             /> }
             {isToggled && <div className="flex flex-col justify-center items-center gap-4">
-                {navItems.map(item => <Link className={`hover:underline underline-offset-8 hover:text-[#a50062]`} to="/">{item}</Link>)}
+                {navItems.map(item => <Link className={`hover:underline underline-offset-8 hover:text-[#D90368]`} to="/">{item}</Link>)}
             </div>}
         </nav>
     )
